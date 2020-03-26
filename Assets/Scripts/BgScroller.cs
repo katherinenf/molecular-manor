e1a3037104da006e15791287ba98e75f07972343
tullrich <timothy.ullrich@gmail.com>
2020-03-26 14:32:17 -0500

    Add scrolling BG to the minigame

---
+++ Assets/Scripts/BgScroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BgScroller : MonoBehaviour
+{
+    public Texture texture;
+    public Vector2 scrollRate;
+
+    RawImage _image;
+
+    void Start()
+    {
+        float aspect = Screen.height / (float)Screen.width;
+        float width = Screen.width / (float)texture.width;
+
+        _image = gameObject.AddComponent<RawImage>();
+        _image.texture = texture;
+        _image.uvRect = new Rect(0.0f, 0.0f, width, width * aspect);
+    }
+
+    void Update()
+    {
+        var rect = _image.uvRect;
+        rect.position = scrollRate * Time.time;
+        _image.uvRect = rect;
+    }
+}
