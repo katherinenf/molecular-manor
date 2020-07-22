8e0ca29ce338021acc1a95b3db7c199a2861d7cd
Katherine NF <Katherine Newport-Forbes>
2020-07-22 12:27:49 -0500

    Trap room exit strategy

--- Assets/Scripts/TrapRoomManager.cs
+++ Assets/Scripts/TrapRoomManager.cs
@@ -77,5 +77,10 @@ public class TrapRoomManager : MonoBehaviour
     {
         fader.FadeOut("HallwayScene");
     }
+
+    public void CloseButton()
+    {
+        missingBox.SetActive(false);
+    }
 }
 
