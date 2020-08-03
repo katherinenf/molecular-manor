87be2a68aad9462c8f5fb2379a4535bf594e1518
tullrich <timothy.ullrich@gmail.com>
2020-08-03 12:12:46 -0500

    Add green/red windows to indicate if a room is complete

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,30 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum RoomType
+{
+    Minigame,
+    Trap
+}
 
 public class Door : MonoBehaviour
 {
+    public RoomType type;
     public MinigameLevel level;
+    public TrapRoom trap;
     public Fader fader;
-    public string type;
     public HallwayManager hallwayManager;
+    public Image windowImage;
+    public Sprite completeSprite;
+
+    public void Start()
+    {
+        // Use the green sprite if this room is complete
+        if (IsComplete())
+        {
+            windowImage.sprite = completeSprite;
+        }
+    }
 
     // assigns level and loads minigame scene
     public void ButtonClicked()
     {
-        if (hallwayManager.doorsClickable)
+        if (hallwayManager.doorsClickable && !IsComplete())
         {
-
-            if (type == "minigame")
+            switch (type)
             {
-                Globals.nextLevel = level;
-                fader.FadeOut("MiniGameScene", transform.position);
-            }
-            if (type == "trap")
-            {
-                fader.FadeOut("TrapRoom1");
+                case RoomType.Minigame: 
+                    {
+                        Globals.nextLevel = level;
+                        fader.FadeOut("MiniGameScene", transform.position);
+                        break;
+                    }
+                case RoomType.Trap:
+                    {
+                        fader.FadeOut("TrapRoom1");
+                        break;
+                    }
             }
         }
     }
 
+    // Returns truthy if the minigame or trap this door is connected too is complete
+    bool IsComplete()
+    {
+        switch (type)
+        {
+            case RoomType.Minigame: return Globals.completeMinigames.Contains(level);
+            case RoomType.Trap: return Globals.completedTraps.Contains(trap);
+        }
+        return false;
+    }
 }
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 public class Globals
 {
     public static MinigameLevel nextLevel;
+
     //texts to go on bottles
     public static List<InventoryItem> inventory = new List<InventoryItem>();
-    public static bool hallwayTutorialCompleted = false;
+    public static bool hallwayTutorialCompleted;
     public static bool minigameTutorialCompleted;
+
+    // Completed rooms
+    public static List<MinigameLevel> completeMinigames = new List<MinigameLevel>();
+    public static List<TrapRoom> completedTraps = new List<TrapRoom>();
 }
