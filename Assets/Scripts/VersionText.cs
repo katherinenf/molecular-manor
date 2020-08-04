b5fd7a66301205618d341af47a3a490bf30b0e29
tullrich <timothy.ullrich@gmail.com>
2020-08-04 13:37:22 -0500

    Add version number to the main menu.

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VersionText : MonoBehaviour
+{
+    void Start()
+    {
+        TMP_Text uiText = GetComponent<TMP_Text>();
+        if (uiText != null)
+        {
+            uiText.text = "v" + Application.version;
+        }
+    }
+}
