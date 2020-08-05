baab6d391dd5e91a79f0ef98abb142f780e05720
tullrich <timothy.ullrich@gmail.com>
2020-08-05 11:43:46 -0500

    Animate the main menu with lightning and birdos

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bat : MonoBehaviour
+{
+    public Sprite[] frames = new Sprite[2];
+    public float frameTime;
+    public float startOffset;
+    Image img;
+    int idx;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        idx = (int)((frames.Length-1) * Mathf.Clamp(startOffset / frameTime, 0f, 1f));
+        while (true)
+        {
+            img.sprite = frames[idx++ % frames.Length];
+            yield return new WaitForSeconds(frameTime + startOffset);
+            startOffset = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject lightning;
+    public Animator bats1;
+    public Animator bats2;
+
+    void Start()
+    {
+        DOTween.Sequence()
+            .AppendInterval(3)
+            .AppendCallback(() => lightning.SetActive(true))
+            .AppendCallback(() => bats1.Play("Fly", -1, 0))
+            .AppendInterval(.08f)
+            .AppendCallback(() => lightning.SetActive(false))
+            .AppendInterval(.12f)
+            .AppendCallback(() => lightning.SetActive(true))
+            .AppendInterval(.08f)
+            .AppendCallback(() => lightning.SetActive(false))
+            .AppendInterval(5)
+            .AppendCallback(() => bats2.Play("Fly", -1, 0))
+            .AppendInterval(3)
+            .SetLoops(-1);
+    }
+}
